dce63871fa7bcc2aae84bffef2a30183f998b688
원치현 <heungeob1003@gmail.com>
2021-07-26 15:17:08 +0900

    최종 수정본

--- js/background.js
+++ js/background.js
@@ -1,13 +1,9 @@
-const images = [
-    "0.jpeg",
-    "1.jpeg",
-    "2.jpeg"
-];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random()*quotes.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src=`img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
